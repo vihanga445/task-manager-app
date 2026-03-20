eef3649bdf9571045d21e926aa4a7467dba97634
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2026-03-20 15:51:13 +0530

    Minor fixes of login and registration pages

--- task-manager-frontend/src/app/components/login/login.component.ts
+++ task-manager-frontend/src/app/components/login/login.component.ts
@@ -2,24 +2,34 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router'; // 1. Import Router
-import { ToastrService } from 'ngx-toastr'; // 1. Import it
+import { Router, RouterModule } from '@angular/router'; 
+import { ToastrService } from 'ngx-toastr'; 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   
+  showPassword = false;
+
   loginForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required])
   });
 
-  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+    private toastr: ToastrService
+  ) {}
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 
   onSubmit() {
     if (this.loginForm.valid) {
